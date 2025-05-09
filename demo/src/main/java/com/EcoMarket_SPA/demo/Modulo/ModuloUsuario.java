7962230aef4bf5a3e0814d291c5e0d88049bbf45
belenisa <bel.alarconh@duocuc.cl>
2025-05-09 13:26:20 -0400

    Update ModuloUsuario.java

--- demo/src/main/java/com/EcoMarket_SPA/demo/Modulo/ModuloUsuario.java
+++ demo/src/main/java/com/EcoMarket_SPA/demo/Modulo/ModuloUsuario.java
@@ -1,5 +1,36 @@
 package com.EcoMarket_SPA.demo.Modulo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.*;
+
+@EntityScan
+@Table(name = "usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ModuloUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+   
+    
+    private String nombre;
+    private String correo;
+    private String fechaNacimiento;
+
+    @ManyToOne
+    @JoinColumn(name = "tipoUsuario_id", nullable = false) // Clave foránea
+    private tipoUsuario_id tipoUsuario;
+
+
+
+    
+
 
 }
