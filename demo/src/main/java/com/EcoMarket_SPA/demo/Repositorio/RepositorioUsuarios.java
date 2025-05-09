bbdbf439b025263a7ffa92c9536b0e9e1cafa856
belenisa <bel.alarconh@duocuc.cl>
2025-05-09 13:26:23 -0400

    Update RepositorioUsuarios.java

--- demo/src/main/java/com/EcoMarket_SPA/demo/Repositorio/RepositorioUsuarios.java
+++ demo/src/main/java/com/EcoMarket_SPA/demo/Repositorio/RepositorioUsuarios.java
@@ -1,7 +1,18 @@
 package com.EcoMarket_SPA.demo.Repositorio;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.EcoMarket_SPA.demo.Modulo.ModuloUsuario;
+
 @Repository
 public class RepositorioUsuarios {
-  private Array<Usuario>
+  private Array<ModuloUsuario> litaUsuarios = new Array<ModuloUsuario>();
+
+  public List<ModuloUsuario> ObtenerUsuario(){
+        return litaUsuarios;
+    }
 
 }
