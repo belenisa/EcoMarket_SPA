f604f228151c2bb6f3dc35760356eea3cfa243a9
belenisa <bel.alarconh@duocuc.cl>
2025-05-18 20:02:40 -0400

    Update Proveedor.java

--- EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/model/Proveedor.java
+++ EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/model/Proveedor.java
@@ -1,6 +1,12 @@
 package com.example.EcoMarket.SPA.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +22,17 @@ public class Proveedor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String nombre;
-
     private String rut;
 
     private String direccion;
 
     private String telefono;
 
-    private String correo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false) // Clave foránea
+    private Usuario usuario;
+
+
+
 }
