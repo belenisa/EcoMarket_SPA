70c05f353d7da234c7d6721c9c222785961f0a92
belenisa <bel.alarconh@duocuc.cl>
2025-05-18 20:05:48 -0400

    Update Usuario.java

--- EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/model/Usuario.java
+++ EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/model/Usuario.java
@@ -1,8 +1,16 @@
 package com.example.EcoMarket.SPA.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,10 +29,28 @@ public class Usuario {
     private String nombre;
     private String correo;
     public enum Rol {
-    ADMIN, CLIENTE, GERENTE, VENTAS, LOGISTICA
+        ADMIN, CLIENTE, GERENTE, VENTAS, LOGISTICA, PROVEEDOR;
+
+        @JsonCreator
+        public static Rol fromString(String value) {
+            if (value == null) return null;
+
+            for (Rol rol : Rol.values()) {
+                if (rol.name().equalsIgnoreCase(value)) { // Permite ADMIN y "admin"
+                    return rol;
+                }
+            }
+            return null; // Evita que Spring falle antes de validar
+        }
+
+        @JsonValue
+        public String toJson() {
+            return name();
+        }
     }
 
     @Enumerated(EnumType.STRING)
     @JsonProperty("rol")
     private Rol rol;
+    
 }
