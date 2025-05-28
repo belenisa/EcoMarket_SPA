628005b44afda501b15f23401766dddf51ebe7e0
belenisa <bel.alarconh@duocuc.cl>
2025-05-28 15:35:20 -0400

    Update PedidoController.java

--- EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/controller/PedidoController.java
+++ EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/controller/PedidoController.java
@@ -13,6 +13,11 @@ public class PedidoController {
 
     @Autowired
     private PedidoService pedidoService;
+    private final String usuarioAutorizado = "CLIENTE";
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
 
     @GetMapping
     public List<Pedido> listarPedidos() {
@@ -20,8 +25,11 @@ public class PedidoController {
     }
 
     @PostMapping
-    public Pedido agregarPedido(@RequestBody Pedido pedido) {
-        return pedidoService.savePedido(pedido);
+    public ResponseEntity<Object> agregarPedido(@RequestBody Pedido pedido, @RequestParam String userRole) {
+        if (!userRole.equals("CLIENTE")) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Solo los clientes pueden agregar pedidos.");
+        }
+        return ResponseEntity.ok(pedidoService.savePedido(pedido));
     }
 
     @GetMapping("/{id}")
@@ -34,6 +42,21 @@ public class PedidoController {
         pedido.setId(id);
         return pedidoService.updatePedido(pedido);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> actualizarPedido(@PathVariable int id, @RequestBody Pedido pedido, @RequestParam String userRole) {
+        if (!(userRole.equals("ADMIN") || userRole.equals("GERENTE") || userRole.equals("VENTAS"))) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Solo ADMIN, GERENTE y VENTAS pueden actualizar pedidos.");
+        }
+
+        if (pedido.getEstado() == null) {
+            return ResponseEntity.badRequest().body("El estado debe ser: PENDIENTE, ENVIADO, ENTREGADO");
+        }
+
+        pedido.setId(id);
+        return ResponseEntity.ok(pedidoService.updatePedido(pedido));
+    }
+
 
     @DeleteMapping("/{id}")
     public String eliminarPedido(@PathVariable int id) {
