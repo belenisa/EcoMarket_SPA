dd081a8aefc18476ab630f5eab8baf11075afffe
belenisa <bel.alarconh@duocuc.cl>
2025-06-16 16:37:40 -0400

    Update ProductoControllerV2.java

--- EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/controller/ProductoControllerV2.java
+++ EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/controller/ProductoControllerV2.java
@@ -13,23 +13,23 @@ import java.util.List;
 public class ProductoControllerV2 {
 
     @Autowired
-    private ProductoService productoService;
+    private ProductoServiceV2 productoServiceV2;
     private final String usuarioAutorizado = "ADMIN";
 
     @GetMapping
     @Operation(summary = "Obtener lista de los productos", description = "Obtiene una lista de todos los productos")
     public List<Producto> listarProductos() {
-        return productoService.getProductos();
+        return productoServiceV2.getProductos();
     }
 
     @PostMapping
     public Producto agregarProducto(@RequestBody Producto producto) {
-        return productoService.saveProducto(producto);
+        return productoServiceV2.saveProducto(producto);
     }
 
     @GetMapping("/{id}")
     public Producto buscarProducto(@PathVariable int id) {
-        return productoService.getProductoId(id);
+        return productoServiceV2.getProductoId(id);
     }
 
     @PutMapping("/{id}")
@@ -38,17 +38,17 @@ public class ProductoControllerV2 {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Solo ADMIN pueden actualizar pedidos.");
         }
         pedido.setId(id);
-        return ResponseEntity.ok(productoService.updatePedido(producto));
+        return ResponseEntity.ok(productoServiceV2.updatePedido(producto));
     }
 
     @DeleteMapping("/{id}")
     public String eliminarProducto(@PathVariable int id) {
-        return productoService.deleteProducto(id);
+        return productoServiceV2.deleteProducto(id);
     }
 
     @GetMapping("/total")
     public int totalProductos() {
-        return productoService.totalProductos();
+        return productoServiceV2.totalProductos();
     }
 }
 
