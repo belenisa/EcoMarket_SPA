0e7b9d5bc2722a7dd6d7fa26c7712575aeb613c6
belenisa <bel.alarconh@duocuc.cl>
2025-06-23 07:33:48 -0400

    Delete ProveedorRepository.java

--- EcoMarket-SPA/EcoMarket-SPA/EcoMarket-SPA/src/main/java/com/example/EcoMarket/SPA/repository/ProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcoMarket.SPA.repository;
-
-import com.example.EcoMarket.SPA.model.Proveedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
-}
